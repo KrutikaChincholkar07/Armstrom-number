84f8b2f17c6f60a1af1ec26db56aeb877848dc08
KrutikaChincholkar07 <chincholkarkrutika07@gmail.com>
2025-09-07 23:54:53 +0530

    Create Armstrong.c
    Armstrong number code in C
    Enter a number: 153
    153 is an Armstrong number.

---
+++ Armstrong.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int num, original, remainder, n = 0;
+    double sum = 0.0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    // Count the number of digits
+    int temp = num;
+    while (temp != 0) {
+        temp /= 10;
+        n++;
+    }
+
+    temp = num;
+    // Calculate the sum of the digits raised to the power n
+    while (temp != 0) {
+        remainder = temp % 10;
+        sum += pow(remainder, n);
+        temp /= 10;
+    }
+
+    // Check if the sum is equal to the original number
+    if (sum == original)
+        printf("%d is an Armstrong number.\n", original);
+    else
+        printf("%d is not an Armstrong number.\n", original);
+
+    return 0;
+}
